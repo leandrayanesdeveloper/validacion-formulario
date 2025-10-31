c26efb2cf8629abd612e759a1d65579ebdbf477f
Leandrayanesdeveloper <Leandrayaneshurtado@gmail.com>
2025-10-31 12:10:32 -0400

    javascript actualizado

--- index.js
+++ index.js
@@ -1,29 +1,43 @@
-console.log('los pollitos dicen: "pio, pio, pio"');
 
+// validacion
 /* nombre de usuario*/
-const NOMBRE_USUARIO = /[a-zA-Z][a-zA-Z0-9-_]{3,32}/gi;
-console.log(NOMBRE_USUARIO.test('Leandra_yanes1'));
-console.log(NOMBRE_USUARIO.test('kiiwj2188---6'));
-
-
+const NOMBRE_USUARIO = /[a-zA-Z][a-zA-Z0-9-_]{4,8}/gi;
 /* email*/
 const EMAIL =/[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/g;
-console.log(EMAIL.test ('leandrayaneshurtad@gmail.com'));
-console.log(EMAIL.test ('leasdi125.com'));
-
-
 /*numero de telefono*/
 const NUMERO_TELEFONO =  /^\s*(?:\+?(\d{1,3}))?([-. (]*(\d{3})[-. )]*)?((\d{3})[-. ]*(\d{2,4})(?:[-.x ]*(\d+))?)\s*$/gm;
-    console.log(NUMERO_TELEFONO.test ('+58 414 858 696'));
-    console.log(NUMERO_TELEFONO.test ('1552lluyy'));
-
-
 /*contraseña*/
 const CONTRASEÑA = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,10}$/gm;
-console.log(CONTRASEÑA.test ('louis123'));
-console.log(CONTRASEÑA.test ('Louis123'));
+
+//selectores 
+const  usernameInput = document.querySelector('#username');
+const countries = document.querySelector('#countries');
+
+//validaciones
+let usernameValidation = false;
+
+/*que solo se muestre el nombre del país y no el código (+)*/
+
+[...countries].forEach(option =>{
+  option.innerHTML =  (option.innerHTML.split('(')[0]);
+})
 
 
+usernameInput.addEventListener('input', e => {
+   usernameValidation = NOMBRE_USUARIO.test(e.target.value);
+   const informacion = e.target.parentElement.children[1];
 
+  if (usernameValidation) {
+    usernameInput.classList.remove('incorrect');
+    usernameInput.classList.add('correct');
+    informacion.classList.remove('show-information');
+  } else {
+    usernameInput.classList.remove('correct');
+    usernameInput.classList.add('incorrect');
+    informacion.classList.add('show-information');
+  }
+ });
 
-
+ 
+ 
+    
